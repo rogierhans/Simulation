b867b11bec8d55b348beb1a0dc3cae72743741dd
rogierhans <rogierhans@gmail.com>
2016-09-20 03:23:38 +0200

    Github maakt het leven makelijk

---
+++ TramSimulator/LibraryRoutine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramSimulator
+{
+    static class LibraryRoutine
+    {
+        static Random random = new Random();
+
+        static public double negexp(double u)
+        {
+            return Math.Log(1 - random.NextDouble()) / (-(1 / u));
+        }
+
+        static public double uniform(double low, double high)
+        {
+            return random.NextDouble() * (high - low) + low;
+        }
+    }
+}
